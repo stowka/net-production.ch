ab8c6be063e4f1c820f19c8e00ac2f22c150a639
Marie <coucoucmarie@hotmail.fr>
2015-03-17 13:55:37 +0100

    "test"

---
+++ www/views/sections/projects.sec.php
@@ -0,0 +1,61 @@
+<!-- Portfolio -->
+	<div class="wrapper style3">
+		<article id="portfolio">
+			<header>
+				<h2>Posuere quis curae vis odio aliquet.</h2>
+				<p>Proin odio consequat  sapien vestibulum consequat lorem dolore feugiat lorem ipsum dolore.</p>
+			</header>
+			<div class="container">
+				<div class="row">
+					<div class="4u">
+						<article class="box style2">
+							<a href="#" class="image featured"><img src="global/img/screenshots/deltapianotrio.png" alt="" /></a>
+							<h3><a href="#">Magna feugiat</a></h3>
+							<p>Ornare nulla proin odio consequat.</p>
+						</article>
+					</div>
+					<div class="4u">
+						<article class="box style2">
+							<a href="#" class="image featured"><img src="global/img/pictures/pic02.jpg" alt="" /></a>
+							<h3><a href="#">Veroeros primis</a></h3>
+							<p>Ornare nulla proin odio consequat.</p>
+						</article>
+					</div>
+					<div class="4u">
+						<article class="box style2">
+							<a href="#" class="image featured"><img src="global/img/pictures/pic03.jpg" alt="" /></a>
+							<h3><a href="#">Lorem ipsum</a></h3>
+							<p>Ornare nulla proin odio consequat.</p>
+						</article>
+					</div>
+				</div>
+				<div class="row">
+					<div class="4u">
+						<article class="box style2">
+							<a href="#" class="image featured"><img src="global/img/pictures/pic04.jpg" alt="" /></a>
+							<h3><a href="#">Tempus dolore</a></h3>
+							<p>Ornare nulla proin odio consequat.</p>
+						</article>
+					</div>
+					<div class="4u">
+						<article class="box style2">
+							<a href="#" class="image featured"><img src="global/img/pictures/pic03.jpg" alt="" /></a>
+							<h3><a href="#">Feugiat aliquam</a></h3>
+							<p>Ornare nulla proin odio consequat.</p>
+						</article>
+					</div>
+					<div class="4u">
+						<article class="box style2">
+							<a href="#" class="image featured"><img src="global/img/pictures/pic02.jpg" alt="" /></a>
+							<h3><a href="#">Sed amet ornare</a></h3>
+							<p>Ornare nulla proin odio consequat.</p>
+						</article>
+					</div>
+				</div>
+			</div>
+			<footer>
+				<p>Lorem ipsum dolor sit sapien vestibulum ipsum primis?</p>
+				<center><a href="#contact" class="button big scrolly">Ata ut neque nisi et</a></center>
+			</footer>
+		</article>
+	</div>
