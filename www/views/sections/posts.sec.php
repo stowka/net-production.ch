27853d32e6e73ec03618380c94a876ed57ac2bea
Arnaud Colin <colin.arnaud9@gmail.com>
2015-03-18 13:43:33 +0100

    Add position, fix img width/height, add default img if path not set

--- www/views/sections/posts.sec.php
+++ www/views/sections/posts.sec.php
@@ -18,12 +18,21 @@
         
         <section class="6u 12u(narrower)">
             <div class="box post">
-                <a href="#" class="image left">
-                    <img src="global/img/portraits/<?php echo $member->getImage();?>" alt="<?php echo $member->getName();?>"/>
-                </a>
+                <?php if ("" != $member->getImage()) {?>
+                    <a href="#" class="image left">
+                        <img src="global/img/portraits/<?php echo $member->getImage();?>" alt="<?php echo $member->getName();?>"/>
+                    </a>
+                <?php } 
+                else { ?>
+                    <a href="#" class="image left">
+                        <img src="global/img/pictures/pic01.jpg" alt="<?php echo $member->getName();?>"/>
+                    </a>
+                <?php } ?>
+
 				<div class="inner">
                 <h3><?= utf8_encode($member->getName())?></h3>
-                    <p><?=utf8_encode($member->getBiography())?></p>
+                <h4><?= utf8_encode($member->getPosition())?></h4>
+                <p><?=utf8_encode($member->getBiography())?></p>
 				</div>
 			</div>
 		</section>
