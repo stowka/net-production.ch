63e8cdae0d11b862e6290843ac09cdabdce018c7
Arnaud Colin <colin.arnaud9@gmail.com>
2015-03-20 09:42:00 +0100

    edit footer links

--- www/views/sections/icons.sec.php
+++ www/views/sections/icons.sec.php
@@ -7,7 +7,7 @@
 ?>
 <!-- Icons -->
 <ul class="icons">
-	<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-	<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-	<li><a href="#" class="icon fa-linkedin"><span class="label">LinkedIn</span></a></li>
+	<li><a href="https://twitter.com/NetProductionCH" target="_blank" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+	<li><a href="https://www.facebook.com/NetProductionKobeAndCo" target="_blank" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+	<li><a href="https://www.linkedin.com/company/5280936" target="_blank" class="icon fa-linkedin"><span class="label">LinkedIn</span></a></li>
 </ul>
