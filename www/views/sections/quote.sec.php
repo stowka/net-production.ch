f2fd3ecf571fe53b776f2df325f8d690574afeea
Nathan Köbe <nathan.kobe@net-production.ch>
2015-03-23 11:37:46 +0100

    Changed quotes back to normal

--- www/views/sections/quote.sec.php
+++ www/views/sections/quote.sec.php
@@ -9,7 +9,7 @@
 <section class="wrapper style2 ">
 	<div class="container">
 		<header class="major">
-			<h2 style="color:#bbb; font-weight: 400">"The internet is becoming the town square 
+			<h2>"The internet is becoming the town square 
 				<br>for the global village of tomorrow"</h2>
 			<p>Bill Gates</p>
 		</header>
