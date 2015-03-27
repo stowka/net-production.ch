06da98bb97b8a35a5f9ab318eb3615ca0317e562
Arnaud Colin <colin.arnaud9@gmail.com>
2015-03-27 09:58:09 +0100

    Add google verification meta

--- www/views/sections/head.sec.php
+++ www/views/sections/head.sec.php
@@ -10,6 +10,7 @@
 	<meta charset="utf-8">	
 	<title><?php echo DEFAULT_TITLE; ?></title>
 	<!-- meta name="viewport" content="width=device-width, initial-scale=1.0"-->
+	<meta name="google-site-verification" content="3tZXe6vrG3TLMx_55JTmOnHLQSpXmu1NhtQu3d1UgAk" />
 
 	<!-- STYLESHEETS -->
 
