82b22abf3508852614fe3100ce16bc44808996e2
MrPwet <beligat.fx@gmail.com>
2015-04-16 14:29:07 +0200

    Fixed logos

--- www/views/sections/projects.sec.php
+++ www/views/sections/projects.sec.php
@@ -39,6 +39,31 @@
 								</li>
 
 							<?php /* FANCYBOX */ ?>
+							<?php elseif ($i <= 3): ?>
+								<li class="4u polaroid">
+									<a 	
+										data-projects='
+										<?php 
+										foreach ($projects as $project): 
+											
+											echo "<li class=\"li-project\" ><a class=\"fancybox\" rel=\"group-" . $type->getId() . "\" href=\"global/img/screenshots/mini/" . str_replace("png", "jpg", str_replace(" ", "%20", $project->getPicture())) . "\" target=\"_blank\"><div class=\"img-project square animated\" >",
+											 
+											 "<img src=\"global/img/screenshots/mini/" . $project->getPicture() . "\" alt=\"" . $project->getPicture() . "\" width=\"30%\">";
+											
+											echo "<div class=\"title-project\" ><h3>" . $project -> getTitle() . "</h3>",
+											"<p class=\"mob-h3-project\">" . $project -> getDescription() . "</p></div>",
+											"</div></a></li>";
+
+										endforeach; ?>'
+										data-largesrc="" 
+										data-title="<?php echo $type->getLabel() ?>">
+											<article class="box style2">
+												<span class="image"><img src="global/img/categories/<?php echo $type->getImage(); ?>" alt="<?php echo $type->getLabel(); ?>"/></span>
+												<h3><?php echo $type->getLabel()?></h3>
+											</article>
+									</a>
+								</li>
+
 							<?php elseif ($i <= 4): ?>
 								<li class="4u polaroid">
 									<a 	
@@ -63,6 +88,7 @@
 											</article>
 									</a>
 								</li>
+
 
 							<?php else: ?>
 								<li class="4u polaroid">
